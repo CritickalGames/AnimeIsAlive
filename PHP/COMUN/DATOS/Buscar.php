502b16e7d2f641e984c5a920a8f2d3a3ae562f1d
Critickal Web <zgtale1@hotmail.com>
2022-12-12 00:47:35 -0300

    ernnr

--- PHP/COMUN/DATOS/Buscar.php
+++ PHP/COMUN/DATOS/Buscar.php
@@ -9,12 +9,31 @@ $nombre=$_POST["nombre"];
 $apellido=$_POST["apellido"];
 $fecha=$_POST["fecha"];
 
+$varT=$obj->buscarDatosPorTelefono($telefono);
+$varN=$obj->buscarDatosPorNombre($nombre);
+$varA=$obj->buscarDatosPorApellido($apellido);
+$varF=$obj->buscarDatosPorFecha($fecha);
 
-$array=var_dump($obj->buscarDatosPorTelefono($telefono));
-echo"<br>";
-$array=var_dump($obj->buscarDatosPorNombre($nombre));
-echo"<br>";
-$array=var_dump($obj->buscarDatosPorApellido($apellido));
-echo"<br>";
-$array=var_dump($obj->buscarDatosPorFecha($fecha));
+$array=array();
+if ($telefono!=NULL) {
+
+    if (in_array($varT[0], $array)) {
+        echo array_diff($varT, $varT[0]);
+        echo "<br>";
+    }
+
+}
+if ($nombre!=NULL) {
+    array_push($array, $varN[0]);
+}
+if (!$apellido!=NULL) {
+    array_push($array, $varA[0]);
+}
+if (!$fecha!=NULL) {
+    array_push($array, $varF[0]);
+}
+var_dump($varT);
+echo "<br>";
+var_dump($array);
+
 ?>
--- prob.php
+++ prob.php
@@ -6,15 +6,47 @@
 //$objModeloAnime         = new ModeloAnime();
 //$objModeloConexion      = new ModeloConexion();
 
+$array = [
+["telefono"=> "1111111", "nombre"=>  "adsqwtb zdfs", "apellido"=>  "ajd 3fdgdc", "fechnac"=>  "0000-00-00" ],
+["telefono"=> "1111111", "nombre"=>  "aaaa", "apellido"=>  "abbab", "fechnac"=>  "2022-12-11" ],
+["telefono"=> "1111111", "nombre"=> "35zd eas", "apellido"=> "124zdzxdg", "fechnac"=> "2022-12-11"]];
 
+$ingresado=["telefono"=> "14234", "nombre"=> "35zd eas", "apellido"=> "124zdzxdg", "fechnac"=> "2022-12-11"];
 
+array_push($array, $ingresado);
+array_push($array, $ingresado);
+
+$resultado=[];
+$claves=[];
+
+for ($i=0; $i <count($array) ; $i++) { 
+    echo "Iniciamos [$i]";
+    echo "<br>";
+    for ($j=count($array)-1; $j >= 0 ; $j--) {
+
+        if (
+            $array[$i]["telefono"]==$array[$j]["telefono"]
+            &&(!in_array($array[$i]["telefono"], $claves))
+        ) {
+            array_push($resultado, $array[$i]);
+            array_push($claves, $array[$i]["telefono"]);
+            
+        }else{
+            echo "$i-$j<br>";
+        }
+    }
+    echo "<br>";
+}
+
+var_dump($resultado);echo"<br>";
+print_r($claves);
 
 ?>
 
 
 <form action="PHP/COMUN/DATOS/Listar.php" method="post">
     
-    <button type="submit">buscar por nombre</button>
+    <button type="submit">Listar</button>
 </form>
 
 
