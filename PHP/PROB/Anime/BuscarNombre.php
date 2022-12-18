22c6046f3e9cfedd09d168be3e3210adba058c79
CritickalGames <31327715+CritickalGames@users.noreply.github.com>
2022-12-18 04:35:40 -0300

    base de datos actualizada con todas las portadas

--- PHP/PROB/Anime/BuscarNombre.php
+++ PHP/PROB/Anime/BuscarNombre.php
@@ -7,10 +7,20 @@ require_once "../../USER/Admin.php";
     if (($nombre!=NULL)) {
         $animes=($objUserAdmin->listarAnimes_nombre($nombre));
         $estados=($objUserAdmin->listarEstados_nombre($nombre));
-        
+        $portadas=($objUserAdmin->listarPortadas_nombre($nombre));
+
         foreach ($animes as $key => $value) {
             $array;
             $array[$key]=$value;
+            foreach ($portadas as $portadaK => $portadaV) {
+                
+                if ($portadaV) {
+                    if (($value["nombre"]==$portadaV["nombre"])) {
+                        $array[$key]=$portadaV;
+                    }else {
+                    }
+                }
+            }
             foreach ($estados as $estadoK => $estadoV) {
                 if ($estadoV) {
                     if (($value["nombre"]==$estadoV["nombre"])) {
